5848dfedde0f38ea7f26acee1fe34bdb3f62b880
Ratnesh Maurya <ratneshmaurya2001@gmail.com>
2022-01-09 22:51:05 +0530

    added redux functionality

--- components/Products.jsx
+++ components/Products.jsx
@@ -2,10 +2,31 @@ import Image from "next/image"
 import { useState } from "react";
 import { StarIcon } from "@heroicons/react/solid";
 
+import { useDispatch } from "react-redux"; //for dispatching to redux store
+import { addToBasket } from "../redux/basketSlice"; //function of redux store for storing products
+
 const Max_rating=5;
 const Min_rating=1;
 
 const Products = ({id,title,price,description,category,image}) => {
+
+    const dispatch = useDispatch();
+    const addItemToBasket=()=>{
+        //now taking our whole products info and dispatching to store
+        const product={
+            id,
+            title,
+            price,
+            rating,
+            description,
+            category,
+            image,
+            hasPrime,
+        };
+
+        //sending the product as an action to the redux store(i.e to basket slice)
+        dispatch(addToBasket(product));
+    }
 
     const[rating]=useState(
         Math.floor(Math.random()*(Max_rating-Min_rating + 1))+Min_rating
@@ -30,13 +51,13 @@ const Products = ({id,title,price,description,category,image}) => {
 
             {/* making random stars rating , making array of ratings and filling with empty values and running map on it to show staricon that mush times*/}
             <div className="flex">
-                {Array(rating).fill().map((i)=>(<StarIcon className="h-5 text-yellow-500"/>))}
+                {Array(rating).fill().map((_,i)=>(<StarIcon key={i} className="h-5 text-yellow-500"/>))}
             </div>
 
             <p className="text-xs my-2 line-clamp-2">{description}</p>
 
             {/* as the price is in euros in API */}
-            <div className="mb-5">{price * 100}</div>
+            <div className="mb-5">₹ {price * 100}</div>
 
             {/* showing prime image for random products */}
             {hasPrime && (
@@ -46,7 +67,7 @@ const Products = ({id,title,price,description,category,image}) => {
                 </div>
             )}
 
-            <button className="mt-auto button">Add to Basket</button>
+            <button onClick={addItemToBasket} className="mt-auto button">Add to Basket</button>
         </div>
     );
 }
