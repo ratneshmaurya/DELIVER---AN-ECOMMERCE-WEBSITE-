2271113881bf01702c70c7f42345a1a0d5ca8921
Ratnesh Maurya <ratneshmaurya2001@gmail.com>
2022-01-13 02:51:14 +0530

    added shopping success confirmation page

---
+++ pages/success.js
@@ -0,0 +1,31 @@
+import Header from '../components/Header'
+import {CheckCircleIcon} from "@heroicons/react/solid"
+import { useRouter } from 'next/router'
+
+const success = () => {
+    const router =useRouter(); 
+
+    return (
+        <div className='bg-gray-100 h-screen'>
+            <Header/>
+
+            <main className='max-w-screen-lg mx-auto'>
+                <div className='flex flex-col p-10 bg-white'>
+                    <div className='flex items-center space-x-3 m-5'>
+                        <CheckCircleIcon className='text-green-500 h-10'/>
+                        <h1 className='text-3xl font-bold'>
+                            Thank you !!
+                            The order is confirmed :-)
+                        </h1>
+                    </div>
+                    <p>
+                        Thank you for shopping with us. We'll send a confirmation once your order has been shipped. Want to check the status of the order(s) please click the link below.
+                    </p>
+                    <button onClick={()=>router.push("/orders")} className='button mt-8'>Go to my orders</button>
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default success
